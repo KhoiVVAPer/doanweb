2c0828eddcb2852f7a07451d7c9ca7a2a4859fe2
trantanphat <p.ketoul@gmail.com>
2018-04-13 12:03:05 +0700

    Bakup CSDL

---
+++ QLHD/Controllers/AdminController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QLHD.Models;
+
+namespace QLHD.Controllers
+{
+    public class AdminController : Controller
+    {
+        dbQLHDDataContext db = new dbQLHDDataContext();
+        // GET: Admin
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult DSHD()
+        {
+            return View(db.Event.ToList());
+        }
+        [HttpGet]
+        public ActionResult ThemMoiHD()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
